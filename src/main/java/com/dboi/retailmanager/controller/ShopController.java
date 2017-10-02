76bf5257fd18113a328cdb4c8de3435e9bea46d9
developeronly <developeronly@gmail.com>
2017-10-02 14:30:47 +0530

    Added another route for fetching all svailable shops

--- src/main/java/com/dboi/retailmanager/controller/ShopController.java
+++ src/main/java/com/dboi/retailmanager/controller/ShopController.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/")
 public class ShopController {
 
+    private List<Shop> shops = new ArrayList<>();
+
     @RequestMapping(value = "shops", method = RequestMethod.POST)
     public Shop addShop(@RequestBody Shop shop) {
+        shops.add(shop);
         return shop;
     }
 
+    @RequestMapping(value = "shops", method = RequestMethod.GET)
+    public List<Shop> list() {
+        return shops;
+    }
 }
--- src/test/java/com/dboi/retailmanager/controller/ShopControllerTest.java
+++ src/test/java/com/dboi/retailmanager/controller/ShopControllerTest.java
@@ -3,8 +3,13 @@ package com.dboi.retailmanager.controller;
 
 import com.dboi.retailmanager.model.Shop;
 import com.dboi.retailmanager.model.ShopAddress;
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class ShopControllerTest {
 
@@ -17,7 +22,19 @@ public class ShopControllerTest {
         Shop shopToBeCreated = new Shop(name, shopAddress);
         ShopController shopController = new ShopController();
         Shop expectedShop = shopController.addShop(shopToBeCreated);
-        Assert.assertEquals(expectedShop, shopToBeCreated);
+        assertEquals(expectedShop, shopToBeCreated);
+    }
+
+    @Test
+    public void listShops() {
+        List<Shop> shops = new ArrayList<>();
+        ShopController shopController = new ShopController();
+        shopController.addShop(new Shop("xyz", new ShopAddress(1, 411043)));
+        shopController.addShop(new Shop("abc", new ShopAddress(2, 411044)));
+        shopController.addShop(new Shop("pqr", new ShopAddress(3, 411045)));
+        shops = shopController.list();
+        assertFalse(shops.isEmpty());
+        assertEquals(3, shops.size());
     }
 
 }
